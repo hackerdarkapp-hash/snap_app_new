ff2d2673a837a0839602ee602bdac0da20aa8bdf
hackerdarkapp-hash <hackerdarkapp@gmail.com>
2026-06-17 19:47:45 +0300

    Remove netlify/functions/download-zip.js

--- netlify/functions/download-zip.js
+++
@@ -1,7 +0,0 @@
-export const handler = async () => {
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ error: 'تحميل الكود غير متاح في هذه البيئة' }),
-  };
-};
